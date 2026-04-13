a0fb252d1389742107085dfed967790d2f4d29c4
ET2545 <B00164666@mytudublin.ie>
2026-04-13 20:19:48 +0100

    registering the new models in admin

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import CandidateProfile, Skill
+from .models import CandidateProfile, Skill, Job, Application
 
 admin.site.register(CandidateProfile)
 admin.site.register(Skill)
+admin.site.register(Job)
+admin.site.register(Application)

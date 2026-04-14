db2dbb1447264b74935a69a23a3ab0c04d761d0c
ET2545 <B00164666@mytudublin.ie>
2026-04-14 14:20:17 +0100

    allowing regular users to register as candidate only

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,3 +5,10 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'role', 'password1', 'password2']
+        
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'CANDIDATE'
+        if commit:
+            user.save()
+        return user

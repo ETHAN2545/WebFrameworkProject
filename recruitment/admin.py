8e963c2091dd84921acbf0776cf4e7093263f275
ET2545 <B00164666@mytudublin.ie>
2026-04-14 12:20:03 +0100

    registering interview in admin

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CandidateProfile, Skill, Job, Application
+from .models import CandidateProfile, Skill, Job, Application, Interview
 
 admin.site.register(CandidateProfile)
 admin.site.register(Skill)
 admin.site.register(Job)
 admin.site.register(Application)
+admin.site.register(Interview)

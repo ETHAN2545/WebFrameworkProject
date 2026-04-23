796f5d577c59b2be9e82ea47f2607d484c6e86a6
ET2545 <B00164666@mytudublin.ie>
2026-04-23 16:05:53 +0100

    creating one more role "Hiring Manager"

--- accounts/models.py
+++ accounts/models.py
@@ -5,11 +5,13 @@ class User(AbstractUser):
     ADMIN = 'ADMIN'
     RECRUITER = 'RECRUITER'
     CANDIDATE = 'CANDIDATE'
+    HIRING_MANAGER = 'HIRING_MANAGER'
 
     ROLE_CHOICES = [
         (ADMIN, 'Admin'),
         (RECRUITER, 'Recruiter'),
         (CANDIDATE, 'Candidate'),
+        (HIRING_MANAGER, 'Hiring Manager'),
     ]
 
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=CANDIDATE)
